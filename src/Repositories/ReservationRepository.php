d00f226844bd1a2d00868bc2dc91fa15c0bff50d
Laura <laura.carraro.simplon@gmail.com>
2024-03-28 09:41:20 +0100

    ajout méthode getAll dans repo + ajout 3 classes

--- src/Repositories/ReservationRepository.php
+++ src/Repositories/ReservationRepository.php
@@ -3,6 +3,8 @@
 namespace src\Repositories;
 
 use src\Models\Database;
+use PDO;
+use Reservation;
 
 class ReservationRepository {
     private $db;
@@ -13,7 +15,10 @@ class ReservationRepository {
         require_once __DIR__ . '/../../config.php';
     }
 
-    public function getAll(){
-        
-    }
+    public function getAll() {
+        $sql = "SELECT * FROM " . PREFIXE . "vercors_reservations;";
+        $this->db->query($sql);
+    
+        return  $this->db->query($sql)->fetchAll(PDO::FETCH_CLASS, Reservation::class);
+      }
 }
